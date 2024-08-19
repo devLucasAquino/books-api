6cbabc5194b4c2bd8a240acc586a61370de925fe
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-19 20:13:19 -0300

    feat: created endpoints in package web, GetBookById, UpdateBook and DeleteBook

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	service "gobooks/internal/services"
 	"net/http"
+	"strconv"
 )
 
 type BookHandlers struct {
@@ -36,4 +37,67 @@ func (h *BookHandlers) CreateBook(w http.ResponseWriter, r *http.Request){
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandlers) GetBookByID(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	book, err := h.service.GetBookById(id)
+	if err != nil {
+		http.Error(w, "failed get book ID", http.StatusInternalServerError)
+		return
+	}
+	if book == nil {
+		http.Error(w, "book not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandlers) UpdateBook(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	var book service.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return
+	}
+	book.ID = id
+
+	if err := h.service.UpdateBook(&book); err != nil {
+		http.Error(w, "failed to update book", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(book)
+
+}
+
+func (h *BookHandlers) DeleteBook(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.service.DeleteBook(id); err != nil {
+		http.Error(w, "failed to delete book", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
