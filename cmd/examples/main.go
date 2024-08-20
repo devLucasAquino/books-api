d8cfb0d4fce65d32223420f0b18e284c00f9315f
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-20 16:58:39 -0300

    feat: created channel to goroutines

--- cmd/examples/main.go
+++ cmd/examples/main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func counter(n int) {
-	for i := range n {
-		fmt.Println(i)
-		time.Sleep(time.Second)
-	}
-}
 
-func main(){
-	go counter(5)
-	go counter(5)
-	counter(5)
+func main(){ // goroutine 1
+	ch := make(chan string) // empty
+
+	// goroutine 2
+	go func(){
+		ch <- "Full Cycle"
+	}()
+
+	msg := <-ch
+	fmt.Println(msg)
 }
