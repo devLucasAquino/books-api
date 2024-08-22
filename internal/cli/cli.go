59cdc12da7d3b1ee7f919c7a410d63fbcc22cc01
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-22 16:53:54 -0300

    feat: added func simulateReading on cli.go

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	service "gobooks/internal/services"
 )
@@ -31,6 +33,14 @@ func (cli *BookCLI) run(){
 		}
 		bookName := os.Args[2]
 		cli.searchBooks(bookName)
+	case "simulate":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: books simulate <book_id> <book_id> <book_id> ...")
+			return
+		}
+		bookIDs := os.Args[2:]
+		cli.simulateReading(bookIDs)
+
 	}
 
 	
@@ -51,7 +61,24 @@ func (cli *BookCLI) searchBooks(name string){
 
 	for _, book := range books{
 		fmt.Printf("ID: %d, Title: %s, Author: %s, Genre: %s\n",
-			book.ID, book.Title, book.Author, book.Genre
+			book.ID, book.Title, book.Author, book.Genre,
 		)
+	}
+}
+
+func (cli *BookCLI) simulateReading(booksIDsSrt []string){
+	var bookIDs []int
+	for _, idStr := range booksIDsSrt{
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			fmt.Println("Invalid book ID: ", idStr)
+			continue
+		}
+		bookIDs = append(bookIDs, id)
+	}
+	responses := cli.service.SimulateMultipleReadings(bookIDs, 2*time.Second)
+
+	for _, response := range responses {
+		fmt.Println(response)
 	}
 }
